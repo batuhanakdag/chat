9d83d78cd48fae79a8c622c1a8fcea31f3ae1dce
batuhanakdag <73172818+batuhanakdag@users.noreply.github.com>
2020-11-09 15:27:25 +0300

    Add files via upload

---
+++ script/ayarlar.js
@@ -0,0 +1,15 @@
+irc.ayarlar.giriscikis = '0'; // Giriş çıkışları gösterir. 0 Açık / 1 Kapalı
+irc.ayarlar.noticegoster = '0'; // Noticeleri gösterir. 0 Açık / 1 Kapalı
+irc.ayarlar.nickdegisim = '1'; // Nick değişikliklerini gösterir. 0 Açık / 1 Kapalı
+irc.ayarlar.banlar = '1'; // Kanalda atılan banları gösterir. 0 Açık / 1 Kapalı
+irc.ayarlar.modeler = '1'; // Kanal mode değişimini gösterir. 0 Açık / 1 Kapalı
+irc.ayarlar.zaman = '0'; // Yazıların başında zamanı gösterir. 0 Açık / 1 Kapalı
+irc.ayarlar.ozeluyari = '0'; // Özelde nickiniz yazınca uyarır. 0 Açık / 1 Kapalı
+irc.ayarlar.kanaluyari = '0'; // Kanalda nickiniz yazılınca uyarır. 0 Açık / 1 Kapalı
+irc.ayarlar.sesler = '0'; // Özelden biri yazınca ses çalar. 0 Açık / 1 Kapalı
+irc.ayarlar.zamanFormat =  'HH:mm'; //Saniye gözüksün isterseniz HH:mm:ss olarak ayarlayın.
+irc.ayarlar.ident =  'yenisite'; //ident değeri girin 
+irc.ayarlar.fullname =  'yenisite IRC Client'; //fullname değeri girin
+irc.ayarlar.kanallar =  '#Radyo'; //Başına # koyarak kanalları ekleyiniz Örnek: #sohbet,#muhabbet
+irc.sayfabasligi = 'Mobil Sohbet, Mobil Chat, Cep Sohbet - GeLKeyfim.Com'; // Sunucuya girince sayfa başlağı bu olacaktır...
+irc.ayarlar.RadyoURL = 'https://radyoserver1.okeylisans.com:14000/7590/;';
